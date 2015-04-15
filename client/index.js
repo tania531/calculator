bab7ca4c43398f8bc015be461e5be6aec3457c0f
Tania <tania.dev77@gmail.com>
2015-04-15 14:29:20 -0700

    key input

--- client/index.js
+++ client/index.js
@@ -1,13 +1,64 @@
 'use strict';
 
-
 $(document).ready(init);
+  function init(){
+}
 
-function init(){
-  $('#enterNumber').click(inputNumbers);
-//  $('#enterNumber').keypress(inputNumbers);
-  $('#getNumbers').keypress(function(){
-    //alert('keys');
+var inputKeys = [];
+
+$(document).keypress(getKeyInput);
+
+function getKeyInput(event){
+  console.log('key in put ', event.which);
+  var k = event.which;
+  k = String.fromCharCode(k);
+
+  if(k==="+"){
+    addKeyNumbers();
+  } else if (k === '*'){
+    multKeyNumbers();
+  }
+  inputKeys.push(k);
+
+  inputKeyVals();
+}
+
+function inputKeyVals(){
+  //alert('inputkeyvales');
+  $('#display').text('Numbers:');
+  inputKeys.forEach(function(n){
+    n = n*1;
+    $('#display').append('<br/>'+n);
+  });
+}
+
+function addKeyNumbers(){
+  var tot=0;
+  inputKeys.reduce(function(a,b){
+    return tot = parseInt(a) + parseInt(b);
+  });
+  $('#total').append('<br />'+tot);
+}
+
+function multKeyNumbers(){
+  var tot=0;
+  inputKeys.reduce(function(a,b){
+    return tot = parseInt(a) * parseInt(b);
+  });
+  $('#total').append('<br />'+tot);
+}
+
+function inputNumbers(){
+  input = $('#getNumbers').val();
+  if(input === '+'){
+    addNumbers();
+  } else if (input === '*'){
+    multNumbers();
+  }
+  nums.push(input);
+  $('#display').text('Numbers:');
+  nums.forEach(function(n){
+    $('#display').append('<br/>'+n);
   });
 }
 
